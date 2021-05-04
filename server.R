aa2fdeb168641bd937721ff55a58e7351cc8e554
mmanry <mmanry@etu.isara.fr>
2021-05-04 16:13:35 +0200

    modif 1 partie exportation

--- server.R
+++ server.R
@@ -22,5 +22,31 @@ shinyServer(function(input, output) {
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
 
     })
-
+    
+    
+    
+    
+    #PARTIE EXPORTATION EN PDF (noms a modifier)
+    #output$report <- downloadHandler(
+        #filename = "report.pdf",
+        #content = function(file) {
+            
+            #tempReport <- file.path(tempdir(), "report.Rmd")
+            #file.copy("report.Rmd", tempReport, overwrite = TRUE)
+            
+            # Set up parameters to pass to Rmd document
+            #params <- list(n = c(input$nompre, input$idDateCre))
+            
+            # Knit the document, passing in the `params` list, and eval it in a
+            # child of the global environment (this isolates the code in the document
+            # from the code in this app).
+            #rmarkdown::render(tempReport, output_file = file,
+                              #params = params,
+                              #envir = new.env(parent = globalenv())
+            #)
+            
+            #output$dateText  <- renderText({
+                #paste("input$date is", as.character(input$date))
+            #})
+        #})
 })
