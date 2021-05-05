8c05b2147b3f111b9b780fca6188f94e9aac608d
benoitbarre <b.barre@etu.isara.fr>
2021-05-05 17:39:22 +0200

    quelque test pour tableau ui.R

--- ui.R
+++ ui.R
@@ -10,24 +10,27 @@ library(shiny)
 library(readxl)
 
 Tabledonnee <- read_excel("Tabledonnee.xls")
+#Tableau<-t(Tabledonnee)
+#colnames(Tableau)<-Tableau[1,]
+#listingre <- length
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
-  titlePanel("NutrISARA"),# title
+  titlePanel("NutrISARA"), # title
   sidebarLayout(
     sidebarPanel("La Calculette Nutritionelle"),
     mainPanel(
       tabsetPanel(
         tabPanel("Introduction",textOutput(outputId= "introduction")),
     
-        tabPanel("La calculette",textOutput(outputId= "calculette"),tableOutput("La Calculette"),
+        tabPanel("La calculette",textOutput(outputId= "calculette"),
              wellPanel(
                 selectInput(inputId = "ingre1", label = "Sélectionner l'ingrédient ",
                             selected = F,choices =c(Tabledonnee$alim_nom_fr)),
                 numericInput(inputId = "gram1", label = "Sélectionner le grammage",
-                             value = 0, min = 0, max = F, step = 5)))
-    ))
+                             value = 0, min = 0, max = F, step = 5))),
+    
+        tabPanel("Analyse",tableOutput("Calctab")))) 
   )
 )
 )
