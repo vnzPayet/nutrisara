0368990a105eacde5048e02fab05de61ba97ae9f
EmmaManson <83407226+EmmaManson@users.noreply.github.com>
2021-05-05 15:23:02 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,14 +1,15 @@
-#
+# Debut script
 # This is the user-interface definition of a Shiny web application. You can
 # run the application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
-#
+# Chargement des fonctions
 library(shiny)
-library(readxl)
+library(readxl)    
 
+# Attribution valeur
 Tabledonnee <- read_excel("Tabledonnee.xls")
 
 shinyUI(fluidPage(
