d63313e64b48403b888d3a0a3535c1f2b1d8ef88
bricelvs <blevasseur@etu.isara.fr>
2021-05-05 19:02:28 +0200

    MAJ de la mis en forme plus debut de rédaction du process d'utlisisation de l'app

--- ui.R
+++ ui.R
@@ -16,19 +16,58 @@ Tabledonnee <- read_excel("Tabledonnee.xls")
 shinyUI(fluidPage(theme = shinytheme("cerulean"),
 
                   titlePanel(tags$img(src="Logo.png",width="450px",height="150px")),
-                  titlePanel("Calculer ce qu'il y a dans son assiette, c'est facile !"),
+                  titlePanel("Calculer la valeur nutritionnelle de son assiette, c'est facile !"),
                   navbarPage(icon("home"),
                   tabPanel(("Introduction"),
-                  textInput(inputId = "intro", label = "Nutrisara est une application qui permet de calculer la valeur nutritionnelle d'un plat ou d'un ingrédient, pour vous permettre d'avoir des assietes équilibrés.", value = "", width =NULL, placeholder =NULL),
-                  textInput(inputId = "Grammage", label = " Par défaut, la valeur sera pour 100g", value = "", width =NULL, placeholder =NULL  ),
-                  textInput(inputId = "recette",label = "Entrer votre recette", value = "", width =NULL, placeholder=NULL),
-                  dateInput(inputId = "IdDate", label = "date de création", value =NULL,min =NULL, max =NULL, format ="yyy-mmm-ddd", startview ="month", weekstart =0, language ="FR"),
-                  dateInput(inputId ="IDdate", label="date de mise à jour",value =NULL, min =NULL, max =NULL,format ="yyy-mmm-ddd", startview ="month", weekstart =0, language ="FR")),
-                  tabPanel("La Calculette"),
+                           fluidRow(
+                                    
+                                    br(),
+                                    p(strong("NutrISARA")," est une application qui permet de calculer la valeur nutritionnelle d'un plat ou d'un ingrédient.", br(),"Pour vous permettre d'avoir des assietes équilibrés."),style="text-align:justify;color:#229954;background-color:#EAECEE;padding:20px;border-radius:10px"),
+                                    br(),
+                  
+                          fluidRow(column(width=2),
+                                   column(
+                                     h4(p("Comment calculer la valeur nutritionnelle de mon assiette ?",style="color:black;text-align:center")),
+                                     width=10,style="background-color:#3498DB;border-radius: 10px")),
+                          br(),
+                          
+                          fluidRow(column(width=2, icon("hand-point-right","fa-5x"),align="center"),
+                                   column(
+                                     br(),
+                                     p(strong("Pour calculer la valeur nutritionnelle"), " de votre assiette, vous aurez besoin de notre calculette !"),style="color:black;text-align:justify",
+                                     width=10,style="background-color:#EAECEE;border-radius: 10px"),
+                                     br(),
+                                     column(
+                                     p("Et pour uiliser notre calculette il vous suffriras d'enregister votre nom :",style="color:black;text-align:justify"),
+                                     p('Nom et Prenom',style="color:grey;border:1px solid black;background-color:white"),
+                                     p("La date du jour :",style="color:black;text-align:justify"),
+                                     p('JJ/MM/AAAA',style="color:grey;border:1px solid black;background-color:white"),
+                                     p("Le nom de la recette :",style="color:black;text-align:justify"),
+                                     p('Nom de la recette',style="color:grey;border:1px solid black;background-color:white"),
+                                     width=10,style="background-color:#EAECEE;border-radius: 10px")),
+                          br(),
+                          fluidRow(
+                                   column(
+                                     p("Cliquez sur l'onglet", strong("Calculette")," pour calculer la valeur nutritionnelle de votre assiette !",style="color:black; text-align:center"),width = 12,style="background-color:#3498DB; border-radius: 10px"
+                                   )),
+                          br(),
+                          fluidRow(column(width = 4),
+                                    column(
+                                      p("Source des données : ",a(href="https://ciqual.anses.fr/"," La base Ciqual"),style="color:black; text-align:center"),width = 4,style="background-color:#EAECEE; border-radius: 10px")),
+                                    br(),
+                                  ),
+                           
+                  tabPanel(("La Calculette"),
+                          textInput(inputId = "Grammage", label = " Par défaut, la valeur sera pour 100g", value = "", width =NULL, placeholder =NULL  ),
+                          textInput(inputId = "recette",label = "Entrer votre recette", value = "", width =NULL, placeholder=NULL),
+                          dateInput(inputId = "IdDate", label = "date de création", value =NULL,min =NULL, max =NULL, format ="yyy-mmm-ddd", startview ="month", weekstart =0, language ="FR"),
+                          dateInput(inputId ="IDdate", label="date de mise à jour",value =NULL, min =NULL, max =NULL,format ="yyy-mmm-ddd", startview ="month", weekstart =0, language ="FR"),
                            wellPanel(
                              selectInput(inputId = "ingre1", label = "Sélectionner l'ingrédient ",
                                          selected = F,choices =c(Tabledonnee$alim_nom_fr)),
                              numericInput(inputId = "gram1", label = "Sélectionner le grammage",
-                                          value = 0, min = 0, max = F, step = 5)))
+                                          value = 0, min = 0, max = F, step = 5)))),
+
+                  
 )
 )
