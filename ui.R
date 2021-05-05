e06dcc7c33ba1416b778076d8d78a1021389830c
bricelvs <blevasseur@etu.isara.fr>
2021-05-05 23:51:29 +0200

    correction APP/Etudiants

--- ui.R
+++ ui.R
@@ -81,7 +81,7 @@ shinyUI(fluidPage(theme = shinytheme("cerulean"),
                            )),
                   
                   hr(),
-                  p(em(strong("Developpé par des APP50 du Module Open ")),
+                  p(em(strong("Developp par des étudiants de la Promo50 : Module Open ")),
                   br("Brice Levasseur Théo Masselis Honorine Favet Marianne Manry Valentine Mouche Célia Devillard Benoit Barré  Mariama Issa Emma MANSON"),style ="text-align:center",
                   br(tags$img(src="téléchargement.png",width="450px",height="150px"))),
                ))      
