f7a3e01d954ee6d27e957b198f9a6436a48add9d
benoitbarre <b.barre@etu.isara.fr>
2021-05-11 10:04:54 +0200

    encore des tests qui fonctionnent pas server.R

--- server.R
+++ server.R
@@ -16,17 +16,60 @@ shinyServer(function(input, output) {
     
     #tab %>% filter(alim_nom_fr==selection) %>% select(2)
     #reactive(input$ingre1)
-        output$tab <-renderTable({tab %>% filter(nom==input$ingre1)}) 
+    tab <- read.csv2("TabTest.csv", dec=",")
+    observe(label = gram1,x = input$gram1)
+    observeEvent(eventExpr = input$ingre1, tab%>%filter(nom==input$ingre1))
+    observeEvent(eventExpr = input$gram1, apply(tab, 1, function(x){5 %*% 2}))
+    output$tab <-renderTable({tab}) #%>% filter(nom==input$ingre1)}) 
+    #output$tab <-renderTable({tab}) %>% apply(tab, 1, function(x){x*input$gram1})})
         #output$tab <-renderTable({tab %>% filter(nom==input$ingre1) %>% select(Eau)})# [input$ingre1, 2]}) #= tab ["input$ingre1", 2]*input$gram1/100 })
         
-    
     #output$listingre <- reactive(ingre1)
     #output$Calctab <- renderTable(Tableau <- filter(Tableau, alim_nom_fr == reactive(ingre1)))
     #output$introduction <-renderText({c("Nutrisara est une application qui permet de calculer la valeur nutritionnelle d'un plat ou d'un argument selon sa quantité, afin d'obtenir une assiette équilibrée")})
     #output$calculette <-renderText({c("Entrer votre plat/recette et définissez vos quantités")})
     
+    ########################################################################################
+    library(dplyr)
     
+    tab <- read.csv2("TabTest.csv", dec=",", encoding = "latin1")
+    head(tab)
+    
+    plat <- tab %>% filter(nom=="Dessert (aliment moyen)")
+    
+    ## Calcul
+    plat$Eau+plat$Protéines
+    
+    ## Modif de plat
+    plat$Eau <- plat$Eau/100*5
+    plat
+    
+    tab %>% filter(nom=="Dessert (aliment moyen)")
+    
+    ## Modif de tab
+    tab %>% filter(nom=="Dessert (aliment moyen)"| nom=="Salade de thon et légumes, appertisée")
+    
+    tab$Eau <- tab$Eau*(1:14)/100
+    tab$Eau*c(0.45,1)
+    
+    ## input 
+    # sélectionner des lignes 
+    ligne <- c(1,3,5)
+    qt <- c(2,3,1,4,5)
+    
+    tabsel <- tab[ligne,2:5]
+    tabsel
+    apply(tabsel, 2, function(x){x*qt})
+    apply(tabsel, 2, function(x){sum(x*qt)})
+    # check
+    0.454*2+2.301*3+3.8
+    
+    ###################################################################################    
 
+    
+    
+    
+    
     #PARTIE EXPORTATION EN PDF (noms a modifier)
 
     output$report <- downloadHandler(
